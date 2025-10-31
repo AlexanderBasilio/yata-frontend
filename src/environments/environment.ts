d6ef66d6a67e6cf21bd8e1031bd6050e73709210
AlexanderBasilio <skillet990710@gmail.com>
2025-10-31 18:13:45 -0500

    Update to custom domain 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  orderServiceUrl: 'https://api.yata-delivery.com',
+  orderServiceUrl: 'https://yata-backend-production.up.railway.app',
   mapbox: {
     accessToken: 'pk.eyJ1IjoieWF0YS10ZWNoLXNlcnZpY2VzIiwiYSI6ImNtaDJ1MWk2MDBobmYybW9mZWZwOGg4cHEifQ.i8C8RnScDWZFaaGAEQy8PA' // Pega tu token aquí
   }
