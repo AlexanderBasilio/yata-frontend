4b71924f6cc809aeaa2d1fa84cad7ea4bef764f3
AlexanderBasilio <skillet990710@gmail.com>
2025-11-13 21:37:03 -0500

    adicion de vercel analytics

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { inject } from '@vercel/analytics';
 
 import { routes } from './app.routes';
 
@@ -11,3 +12,5 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withFetch())
   ]
 };
+
+inject();
